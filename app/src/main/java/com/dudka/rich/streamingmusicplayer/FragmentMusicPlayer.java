54f498d5728d83a20ce6c10f6dd1f68c936a7b9b
Rich Dudka <savvywit@gmail.com>
2016-01-07 08:49:13 -0800

    Added Apache license

--- app/src/main/java/com/dudka/rich/streamingmusicplayer/FragmentMusicPlayer.java
+++ app/src/main/java/com/dudka/rich/streamingmusicplayer/FragmentMusicPlayer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Rich Dudka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dudka.rich.streamingmusicplayer;
 
 import android.app.Activity;
@@ -18,6 +34,8 @@ import android.widget.TextView;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
+ * Created by rich on 12/22/15.
+ *
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentMusicPlayer#newInstance} factory method to
  * create an instance of this fragment.
